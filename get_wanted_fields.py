ae419c29488549c43dbcbb919e61900304e139b0
Alex Scott <ascott011uk@gmail.com>
2025-08-21 22:14:18 -0400

    get keys script made. use these keys for final data so not grabbing everything

---
+++ get_wanted_fields.py
@@ -0,0 +1,32 @@
+import requests
+
+# this file is to see what fields there are so I don't grab everything 
+
+url = 'https://api.openbrewerydb.org/v1/breweries'
+
+def get_wanted_fields():
+    try:
+        response = requests.get(url) 
+
+        if response.status_code == 200:
+            data = response.json()
+        
+            if isinstance(data, list) and len(data) > 0:
+                first_item = data[0]
+                keys = list(first_item.keys())
+                print("Keys:", keys)
+        else:
+            print('Call failed. Error:', response.status_code)
+
+    except requests.exceptions.RequestException as e:
+        print('Error:', e)
+
+    finally:
+        print('Keys retrieved successfully.')       
+
+if __name__ == "__main__":
+    fields = get_wanted_fields()
+    print(fields)
+
+        # wanted_fields = ['id', 'name', 'brewery_type', 'city', 'state', 'postal_code', 'country']
+        # filtered_data = [for key in wanted_fields if key in first_item]
